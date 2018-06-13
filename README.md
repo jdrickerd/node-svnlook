bfcb8d97221e8479b573a5c675d6198511d505f3
Josiah Rickerd <jdrickerd@gmail.com>
2018-06-13 16:04:57 -0400

    Added code files

--- README.md
+++ README.md
@@ -1,2 +1,294 @@
 # node-svnlook
-A NodeJs wrapper for the svnlook utility.
+
+A NodeJs wrapper for the svnlook utility. Contains all the methods exposed by the command line svnlook tool.
+
+```
+npm install node-svnlook --save
+```
+
+Example usage
+
+```
+var svnlook = require('node-svnlook');
+
+svnlook.changed('/my/svn/repo', 
+	{	// optional options object - can be passed to any command not just update
+		shell: "sh", 			// override shell used to execute command
+		cwd: process.cwd(),		// override working directory command is executed
+		revision: 33050,		// provide --revision to commands that accept it
+		transaction: ax9,		// provide --transaction to commands that accept it
+		params: [ '--copy-info' ] // extra parameters to pass
+	},
+    function(err, output) {
+        console.log(output);
+    });
+```
+
+## Functions
+
+<dl>
+<dt><a href="#author">author(repoPath, [options], [callback])</a></dt>
+<dd><p>Print the author of a revision or transaction in the repository.</p>
+</dd>
+<dt><a href="#cat">cat(repoPath, target, [options], [callback])</a></dt>
+<dd><p>Print the contents of a file.</p>
+</dd>
+<dt><a href="#changed">changed(repoPath, [options], [callback])</a></dt>
+<dd><p>Print the paths that were changed in a particular revision or transaction.</p>
+</dd>
+<dt><a href="#date">date(repoPath, [options], [callback])</a></dt>
+<dd><p>Print the datestamp of a revision or transaction in a repository.</p>
+</dd>
+<dt><a href="#diff">diff(repoPath, [options], [callback])</a></dt>
+<dd><p>Print GNU-style differences of changed files and properties in a repository.</p>
+</dd>
+<dt><a href="#dirsChanged">dirsChanged(repoPath, [options], [callback])</a></dt>
+<dd><p>Print the directories that were themselves changed (property edits) or whose file children were changed.</p>
+</dd>
+<dt><a href="#filesize">filesize(repoPath, target, [options], [callback])</a></dt>
+<dd><p>Print the size (in bytes) of a versioned file.</p>
+</dd>
+<dt><a href="#history">history(repoPath, target, [options], [callback])</a></dt>
+<dd><p>Print information about the history of a path in the repository (or the root directory if no path is supplied).</p>
+</dd>
+<dt><a href="#info">info(repoPath, [options], [callback])</a></dt>
+<dd><p>Print the author, datestamp, log message size, and log message.</p>
+</dd>
+<dt><a href="#lock">lock(repoPath, target, [options], [callback])</a></dt>
+<dd><p>If a lock exists on a path in the repository, describe it.</p>
+</dd>
+<dt><a href="#log">log(repoPath, [options], [callback])</a></dt>
+<dd><p>Print the log message.</p>
+</dd>
+<dt><a href="#propget">propget(propName, repoPath, target, [options], [callback])</a></dt>
+<dd><p>List the value of a property on a path in the repository.</p>
+</dd>
+<dt><a href="#proplist">proplist(repoPath, target, [options], [callback])</a></dt>
+<dd><p>List the properties of a path in the repository.</p>
+</dd>
+<dt><a href="#tree">tree(repoPath, target, [options], [callback])</a></dt>
+<dd><p>Print the tree.</p>
+</dd>
+<dt><a href="#uuid">uuid(repoPath, [options], [callback])</a></dt>
+<dd><p>Print the repository&#39;s UUID.</p>
+</dd>
+<dt><a href="#youngest">youngest(repoPath, [options], [callback])</a></dt>
+<dd><p>Print the youngest revision number.</p>
+</dd>
+</dl>
+
+<a name="author"></a>
+
+## author(repoPath, [options], [callback])
+Print the author of a revision or transaction in the repository.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="cat"></a>
+
+## cat(repoPath, target, [options], [callback])
+Print the contents of a file.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| target | <code>string</code> | Path in repo to target |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="changed"></a>
+
+## changed(repoPath, [options], [callback])
+Print the paths that were changed in a particular revision or transaction.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="date"></a>
+
+## date(repoPath, [options], [callback])
+Print the datestamp of a revision or transaction in a repository.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="diff"></a>
+
+## diff(repoPath, [options], [callback])
+Print GNU-style differences of changed files and properties in a repository.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="dirsChanged"></a>
+
+## dirsChanged(repoPath, [options], [callback])
+Print the directories that were themselves changed (property edits) or whose file children were changed.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="filesize"></a>
+
+## filesize(repoPath, target, [options], [callback])
+Print the size (in bytes) of a versioned file.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| target | <code>string</code> | Path in repo to target |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="history"></a>
+
+## history(repoPath, target, [options], [callback])
+Print information about the history of a path in the repository (or the root directory if no path is supplied).
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| target | <code>string</code> | Path in repo to target |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="info"></a>
+
+## info(repoPath, [options], [callback])
+Print the author, datestamp, log message size, and log message.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="lock"></a>
+
+## lock(repoPath, target, [options], [callback])
+If a lock exists on a path in the repository, describe it.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| target | <code>string</code> | Path in repo to target |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="log"></a>
+
+## log(repoPath, [options], [callback])
+Print the log message.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="propget"></a>
+
+## propget(propName, repoPath, target, [options], [callback])
+List the value of a property on a path in the repository.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| propName | <code>string</code> | Property name |
+| repoPath | <code>string</code> | Path to repository |
+| target | <code>string</code> | Path in repo to target |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="proplist"></a>
+
+## proplist(repoPath, target, [options], [callback])
+List the properties of a path in the repository.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| target | <code>string</code> | Path in repo to target |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="tree"></a>
+
+## tree(repoPath, target, [options], [callback])
+Print the tree.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| target | <code>string</code> | Path in repo to target |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="uuid"></a>
+
+## uuid(repoPath, [options], [callback])
+Print the repository's UUID.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
+
+<a name="youngest"></a>
+
+## youngest(repoPath, [options], [callback])
+Print the youngest revision number.
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| repoPath | <code>string</code> | Path to repository |
+| [options] | <code>object</code> | Options object |
+| [callback] | <code>function</code> | Complete callback |
---
+++ index.js
@@ -0,0 +1,358 @@
+/*jslint node: true */
+'use strict';
+
+var exec = require('child_process').exec;
+var os = require('os');
+
+var execute = function (cmd, options, callback) {
+	options = options || {};
+	if (options.shell === undefined && os.platform === 'win32') {
+		options.shell = 'start "" /B ';  // windows only - this makes it so a cmd window won't pop up when running as a service or through pm2
+	}
+	options.cwd = options.cwd || process.cwd();
+	var execOptions = {
+		cwd: options.cwd,
+		shell: options.shell,
+		maxBuffer: options.maxBuffer || (5 * 1024 * 1024) // defaults to 5MB
+	};
+	if (options.params) {
+		cmd += ' ' + options.params.join(' ');
+	}
+	exec(cmd, execOptions, function (err, stdo) {
+		if (typeof callback === 'function') {
+			callback(err, options.stdoutAsBuffer ? stdo : stdo.toString());
+		}
+	});
+};
+
+var executeSvnlook = function (params, options, callback) {
+	options = options || {};
+	var cmd = 'svnlook ' + params.join(' ');
+	execute(cmd, options, callback);
+};
+
+var addExtraOptions = function (validOptionsArray, options, addRevProp) {
+	if (options) {
+		options.params = options.params || [];
+		validOptionsArray.forEach(function (validOption) {
+			switch (validOption) {
+				case 'revision':
+					if (options.revision) {
+						options.params.push('--revision', options.revision.toString());
+						if (addRevProp) {
+							options.params.push('--revprop');
+						}
+					}
+					break;
+				case 'transaction':
+					if (options.transaction) {
+						options.params.push('--transaction', options.transaction.toString());
+					}
+					break;
+				case 'limit':
+					if (options.limit) {
+						options.params.push('--limit', options.limit.toString());
+					}
+					break;
+				case 'extensions':
+					if (options.extensions) {
+						if (options.extensions) {
+							options.params.push('--extensions', options.extensions.toString());
+						}
+						else {
+							options.params.push('--extensions', '--unified');
+						}
+					}
+					break;
+			}
+		});
+	}
+	return options;
+};
+
+/** Print the author of a revision or transaction in the repository.
+ * @function author
+ * @param {string} repoPath - Path to repository
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ */
+var author = function (repoPath, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	addExtraOptions(['revision', 'transaction'], options);
+	executeSvnlook(['author', repoPath], options, callback);
+};
+exports.author = author;
+
+/** Print the contents of a file.
+ * @function cat
+ * @param {string} repoPath - Path to repository
+ * @param {string} target - Path in repo to target
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ */
+var cat = function (repoPath, target, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	addExtraOptions(['revision', 'transaction'], options);
+	executeSvnlook(['cat', repoPath, target], options, callback);
+};
+exports.cat = cat;
+
+/** Print the paths that were changed in a particular revision or transaction.
+ * @function changed
+ * @param {string} repoPath - Path to repository
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ */
+var changed = function (repoPath, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	addExtraOptions(['revision', 'transaction'], options);
+	executeSvnlook(['changed', repoPath], options, callback);
+};
+exports.changed = changed;
+
+/** Print the datestamp of a revision or transaction in a repository.
+ * @function date
+ * @param {string} repoPath - Path to repository
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ */
+var date = function (repoPath, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	addExtraOptions(['revision', 'transaction'], options);
+	executeSvnlook(['date', repoPath], options, callback);
+};
+exports.date = date;
+
+/** Print GNU-style differences of changed files and properties in a repository.
+ * @function diff
+ * @param {string} repoPath - Path to repository
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ */
+var diff = function (repoPath, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	addExtraOptions(['revision', 'transaction', 'extensions'], options);
+	executeSvnlook(['diff', repoPath], options, callback);
+};
+exports.diff = diff;
+
+/** Print the directories that were themselves changed (property edits) or whose file children were changed.
+ * @function dirsChanged
+ * @param {string} repoPath - Path to repository
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ */
+var dirsChanged = function (repoPath, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	addExtraOptions(['revision', 'transaction'], options);
+	executeSvnlook(['dirs-changed', repoPath], options, callback);
+};
+exports.dirsChanged = dirsChanged;
+
+/** Print the size (in bytes) of a versioned file.
+ * @function filesize
+ * @param {string} repoPath - Path to repository
+ * @param {string} target - Path in repo to target
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ */
+var filesize = function (repoPath, target, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	addExtraOptions(['revision', 'transaction'], options);
+	executeSvnlook(['filesize', repoPath, target], options, callback);
+};
+exports.filesize = filesize;
+
+/** Print information about the history of a path in the repository (or the root directory if no path is supplied).
+ * @function history
+ * @param {string} repoPath - Path to repository
+ * @param {string} target - Path in repo to target
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ */
+var history = function (repoPath, target, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	addExtraOptions(['revision', 'limit'], options);
+	executeSvnlook(['history', repoPath, target], options, callback);
+};
+exports.history = history;
+
+/** Print the author, datestamp, log message size, and log message.
+ * @function info
+ * @param {string} repoPath - Path to repository
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ */
+var info = function (repoPath, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	addExtraOptions(['revision', 'transaction'], options);
+	executeSvnlook(['info', repoPath], options, callback);
+};
+exports.info = info;
+
+/** If a lock exists on a path in the repository, describe it.
+ * @function lock
+ * @param {string} repoPath - Path to repository
+ * @param {string} target - Path in repo to target
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ */
+var lock = function (repoPath, target, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	executeSvnlook(['lock', repoPath, target], options, callback);
+};
+exports.lock = lock;
+
+/** Print the log message.
+ * @function log
+ * @param {string} repoPath - Path to repository
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ */
+var log = function (repoPath, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	addExtraOptions(['revision', 'transaction'], options);
+	executeSvnlook(['log', repoPath], options, callback);
+};
+exports.log = log;
+
+/** List the value of a property on a path in the repository.
+ * @function propget
+ * @param {string} propName - Property name
+ * @param {string} repoPath - Path to repository
+ * @param {string} target - Path in repo to target
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ * @alias pget
+ * @alias pg
+ */
+var propget = function (propName, repoPath, target, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	addExtraOptions(['revision', 'transaction'], options, true);
+	executeSvnlook(['propget', repoPath, propName, target], options, callback);
+};
+exports.propget = propget;
+exports.pget = propget;
+exports.pg = propget;
+
+/** List the properties of a path in the repository.
+ * @function proplist
+ * @param {string} repoPath - Path to repository
+ * @param {string} target - Path in repo to target
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ * @alias plist
+ * @alias pl
+ */
+var proplist = function (repoPath, target, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	addExtraOptions(['revision', 'transaction'], options, true);
+	executeSvnlook(['proplist', repoPath, target], options, callback);
+};
+exports.proplist = proplist;
+exports.plist = proplist;
+exports.pl = proplist;
+
+/** Print the tree.
+ * @function tree
+ * @param {string} repoPath - Path to repository
+ * @param {string} target - Path in repo to target
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ * @alias plist
+ * @alias pl
+ */
+var tree = function (repoPath, target, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	addExtraOptions(['revision', 'transaction'], options);
+	executeSvnlook(['tree', repoPath, target], options, callback);
+};
+exports.tree = tree;
+
+/** Print the repository's UUID.
+ * @function uuid
+ * @param {string} repoPath - Path to repository
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ */
+var uuid = function (repoPath, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	executeSvnlook(['uuid', repoPath], options, callback);
+};
+exports.uuid = uuid;
+
+/** Print the youngest revision number.
+ * @function youngest
+ * @param {string} repoPath - Path to repository
+ * @param {object} [options] - Options object
+ * @param {function} [callback] - Complete callback
+ */
+var youngest = function (repoPath, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = null;
+	}
+	options = options || {};
+	executeSvnlook(['youngest', repoPath], options, callback);
+};
+exports.youngest = youngest;
